04067562294f4da16170e44864c632fdbd7fab82
Sam Carbine <scarbine@me.com>
2021-06-11 20:47:55 -0500

    initial commit

---
+++ src/components/location/Locations.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Location.css";
+
+export const LocationCard = () => (
+  <section className="location">
+    <h3 className="location__name">Nashville Kennels</h3>
+    <div className="location__area">North</div>
+  </section>
+);
+
+export const LocationCard2 = () => (
+  <section className="location">
+    <h3 className="location__name">Nashville Kennels</h3>
+    <div className="location__area">South</div>
+  </section>
+);
+
+export const LocationCard3 = () => (
+  <section className="location">
+    <h3 className="location__name">Nashville Kennels</h3>
+    <div className="location__area">East</div>
+  </section>
+);
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { Kennel } from "./components/Kennel"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Kennel />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
